0a50477be0a2ef1cc8878598341e703fe9f385d9
Tomas Kravcik <root@Laptop-i7>
2024-10-11 19:36:32 +0200

    implement sound effect with local storage

--- src/components/navigation/SoundButton.jsx
+++ src/components/navigation/SoundButton.jsx
@@ -1,26 +1,78 @@
 "use client"
 import Link from "next/link";
-import {AudioLines} from "lucide-react";
+import {AudioLines, Volume2, VolumeX} from "lucide-react";
 import {motion} from "framer-motion";
+import {useEffect, useRef, useState} from "react";
+
+const Modal = ({onClose, toggle}) => {
+
+}
+
 
 const SoundButton = () => {
 
+    const [isPlaying, setIsPlaying] = useState(false)
+    const audioRef = useRef(null)
 
-    const SoundBtnLink = motion(Link)
+
+    const handleInteraction = () => {
+        const consent = localStorage.getItem("musicConsent")
+        if (consent === "true" && !isPlaying) {
+            setIsPlaying(true)
+            audioRef.current.play()
+        }
+        ['click', "keydown", "touchstart"].forEach((event) => {
+            document.removeEventListener(event, handleInteraction)
+        })
+    }
+
+    const toggleSound = () => {
+        const newState = !isPlaying
+        setIsPlaying(!isPlaying)
+        newState ? audioRef.current.play() : audioRef.current.pause()
+        localStorage.setItem("musicConsent", String(newState))
+    }
+
+    useEffect(() => {
+        const consent = localStorage.getItem("musicConsent")
+        if (consent) {
+            setIsPlaying(consent === "true")
+
+            if (consent === "true") {
+                ['click', "keydown", "touchstart"].forEach((event) => {
+                    document.addEventListener(event, handleInteraction)
+                })
+            }
+        }
+    }, []);
+
     return (
-        <SoundBtnLink href={'/'}
-
-                      initial={{scale: 0}}
-                      animate={{scale: 1}}
-                      transition={{delay: 1}}
-                      className={'text-foreground rounded-full flex items-center justify-center bg-background/20 border border-accent/30 border-solid backdrop-blur-[6px] shadow-glass-inset hover:shadow-glass-inset-sm fixed top-4 right-2.5 xs:right-4 w-fit self-start z-50'}
-                      aria-label={'audio button'}
-                      name={'audio button'}>
-            <audio loop >
-                <source src={'/public/audio/lazy-day.mp3'}></source>
+        <div
+            className={'fixed top-4 right-2.5 xs:right z-50 group'}
+        >
+            <audio loop ref={audioRef}>
+                <source src={'/audio/lazy-day.mp3'} type={'audio/mpeg'}/>
+                Audio not supported
             </audio>
+            <motion.button
+                initial={{scale: 0}}
+                animate={{scale: 1}}
+                transition={{delay: 1}}
 
-        </SoundBtnLink>
+                onClick={toggleSound}
+
+                className={'text-foreground rounded-full flex items-center justify-center bg-background/20 border border-accent/30 border-solid backdrop-blur-[6px] shadow-glass-inset hover:shadow-glass-inset-sm fixed top-4 right-4 w-fit self-start z-50 p-2.5 xs:p-4'}
+            >
+                {
+                    isPlaying ?
+                        <Volume2 className={'text-foreground w-full h-full group-hover:text-accent'} strokeWidth={1.5}/>
+                        :
+                        <VolumeX className={'text-foreground w-full h-full group-hover:text-accent'} strokeWidth={1.5}/>
+                }
+            </motion.button>
+
+
+        </div>
     )
 }
 
